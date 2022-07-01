c60d6cadce49fbc437104416760c407af226b8fb
UmarWahab123 <79415062+UmarWahab123@users.noreply.github.com>
2022-07-01 19:01:38 +0500

    worked on api's

--- src/views/category/Filterproducts.vue
+++ src/views/category/Filterproducts.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div class="col col-12 col-lg-9 content-primary">
     <div class="products column-4 mobile-2" v-if="isCount">
       <div
@@ -19,6 +20,7 @@
           custom-hover
         "
         v-for="(item, index) in results"
+
       >
         <div class="product-wrapper product-type-1">
           <div class="product-content">
@@ -229,6 +231,8 @@
             </div>
           </div>
         </div>
+      
+
         <div class="product-content-fade" style="margin-bottom: -109px"></div>
         <div
           class="
@@ -262,8 +266,274 @@
           <div class="tinvwl-tooltip">Add to Wishlist</div>
         </div>
       </div>
+
+      <div
+        class="
+          product
+          type-product
+          post-521
+          status-publish
+          first
+          instock
+          product_cat-apple product_cat-cell-phones
+          has-post-thumbnail
+          sale
+          featured
+          shipping-taxable
+          purchasable
+          product-type-simple
+          custom-hover
+        "
+        v-for="(items, indextr) in filterresults"
+      >
+        <div class="product-wrapper product-type-1">
+          <div class="product-content">
+            <div class="thumbnail-wrapper">
+              <div class="product-badges">
+                <span class="badge onsale">11%</span>
+              </div>
+              <a href="javascript:void(0)">
+                <div class="product-card" style="position: relative">
+                  <div
+                    class="hover-slider-images-toggler"
+                    style="position: absolute; inset: 0px; display: flex"
+                  >
+                    <div
+                      class="hover-slider-toggle-pane"
+                      data-hover-slider-image="/wp-content/uploads/2021/09/single-1-400x400.jpg"
+                      data-hover-slider-i="1"
+                      style="flex-grow: 1"
+                    ></div>
+                    <div
+                      class="hover-slider-toggle-pane"
+                      data-hover-slider-image="/wp-content/uploads/2021/09/single-2-400x400.jpg"
+                      data-hover-slider-i="2"
+                      style="flex-grow: 1"
+                    ></div>
+                    <div
+                      class="hover-slider-toggle-pane"
+                      data-hover-slider-image="/wp-content/uploads/2021/09/single-3-400x400.jpg"
+                      data-hover-slider-i="3"
+                      style="flex-grow: 1"
+                    ></div>
+                    <div
+                      class="hover-slider-toggle-pane"
+                      data-hover-slider-image="/wp-content/uploads/2021/09/air3-400x400.jpg"
+                      data-hover-slider-i="4"
+                      style="flex-grow: 1"
+                    ></div>
+                    <div
+                      class="hover-slider-toggle-pane"
+                      data-hover-slider-image="/wp-content/uploads/2021/09/air4-400x400.jpg"
+                      data-hover-slider-i="5"
+                      style="flex-grow: 1"
+                    ></div>
+                  </div>
+                  <div class="hover-slider-indicator">
+                    <div
+                      data-hover-slider-i="1"
+                      class="hover-slider-indicator-dot active"
+                    ></div>
+                    <div
+                      data-hover-slider-i="2"
+                      class="hover-slider-indicator-dot"
+                    ></div>
+                    <div
+                      data-hover-slider-i="3"
+                      class="hover-slider-indicator-dot"
+                    ></div>
+                    <div
+                      data-hover-slider-i="4"
+                      class="hover-slider-indicator-dot"
+                    ></div>
+                    <div
+                      data-hover-slider-i="5"
+                      class="hover-slider-indicator-dot"
+                    ></div>
+                  </div>
+                  <img
+                    src="https://klbtheme.com/machic/wp-content/uploads/2021/09/single-1-400x400.jpg"
+                    data-hover-slides="/wp-content/uploads/2021/09/single-2-400x400.jpg,/wp-content/uploads/2021/09/single-3-400x400.jpg,/wp-content/uploads/2021/09/air3-400x400.jpg,/wp-content/uploads/2021/09/air4-400x400.jpg"
+                    data-options='{"touch": "end", "preloadImages": true }'
+                    alt="Apple 10.9-inch iPad Air Wi-Fi Cellular 64GB"
+                    class="hover-slider-init"
+                  />
+                </div>
+              </a>
+
+              <div class="product-buttons">
+                <div
+                  class="
+                    tinv-wraper
+                    woocommerce
+                    tinv-wishlist
+                    tinvwl-shortcode-add-to-cart
+                  "
+                  data-product_id="478"
+                >
+                  <div class="tinv-wishlist-clear"></div>
+
+                  <a
+                    :onclick="clickmodal"
+                    role="button"
+                    tabindex="0"
+                    aria-label="Add to Wishlist"
+                    class="d-none tinvwl-position-after"
+                    data-tinv-wl-list="[]"
+                    data-tinv-wl-product="478"
+                    data-tinv-wl-productvariation="0"
+                    data-tinv-wl-productvariations="[0]"
+                    data-tinv-wl-producttype="simple"
+                    data-tinv-wl-action="add"
+                    ><span class="tinvwl_add_to_wishlist-text"
+                      >Add to Wishlist</span
+                    ></a
+                  >
+                  <a :onclick="clickmodal" href="javascript::void(0)"
+                    ><i class="fa fa-heart" aria-hidden="true"></i
+                    ><span class="tinvwl_add_to_wishlist-text"
+                      >Add to Wishlist</span
+                    ></a
+                  >
+
+                  <div class="tinv-wishlist-clear"></div>
+                  <div class="tinvwl-tooltip">Add to Wishlist</div>
+                </div>
+                <a href="#?featured=yes" class="most-comments"
+                  ><img
+                    src="https://klbtheme.com/machic/wp-content/themes/machic/assets/images/featured.png"
+                    alt="featured" /></a
+                ><a
+                  href="javascript::void(0)"
+                  class="woosc-btn woosc-btn-521 woosc-btn-added woosc-added"
+                  :onclick="clickcomparemodal"
+                  data-id="521"
+                  >Compare</a
+                ><span
+                  :onclick="clickbigmodal"
+                  class="detail-bnt quickview animated"
+                  ><i class="klbth-icon-eye-empty"></i
+                ></span>
+              </div>
+            </div>
+            <div class="content-wrapper">
+              <h3 class="product-title">
+                <router-link
+                  :to="'/' + langCode + '/product-detail/' + item?.slug"
+                  >{{ items?.product_name }}</router-link
+                >
+              </h3>
+              <div class="product-rating">
+                <div
+                  class="star-rating"
+                  role="img"
+                  aria-label="Rated 5.00 out of 5"
+                >
+                  <span style="width: 100%"
+                    >Rated <strong class="rating">5.00</strong> out of 5</span
+                  >
+                </div>
+                <div class="count-rating">
+                  {{ items?.review_count
+                  }}<span class="rating-text">Ratings</span>
+                </div>
+              </div>
+              <div class="product-price-cart">
+                <span class="price"
+                  ><del aria-hidden="true"
+                    ><span class="woocommerce-Price-amount amount">
+                      <bdi
+                        ><span class="woocommerce-Price-currencySymbol"
+                          >{{ $t("QAR") }} </span
+                        >{{ items?.uom_products?.previous_price }}</bdi
+                      >
+                    </span></del
+                  >
+                  <ins
+                    ><span
+                      class="woocommerce-Price-amount amount"
+                      style="margin: 2px"
+                    >
+                      <bdi
+                        ><span class="woocommerce-Price-currencySymbol"
+                          >{{ $t("QAR") }} </span
+                        >{{ item?.uom_products?.regular_price }}</bdi
+                      >
+                    </span></ins
+                  ></span
+                ><a
+                  href="javascript:void(0)"
+                  data-quantity="1"
+                  class="
+                    button
+                    product_type_simple
+                    add_to_cart_button
+                    ajax_add_to_cart
+                  "
+                  v-if="ButtonCheck"
+                  @click="addtoCart(item, index)"
+                  ><i class="klbth-icon-shop-1"></i> Add to cart</a
+                >
+                <a
+                  v-if="item?.check"
+                  href="javascript:void(0)"
+                  class="added_to_cart wc-forward"
+                  title="View cart"
+                  >View cart</a
+                >
+              </div>
+            </div>
+          </div>
+          <div class="product-footer">
+            <div class="product-footer-details">
+              <ul>
+                <li class="SpecHighlights-list-label">Screen Size 10.9 in</li>
+                <li>Operating System Apple iOS</li>
+                <li>Product Length 9.74 in</li>
+              </ul>
+              &nbsp;
+            </div>
+          </div>
+        </div>
+      
+
+        <div class="product-content-fade" style="margin-bottom: -109px"></div>
+        <div
+          class="
+            tinv-wraper
+            woocommerce
+            tinv-wishlist
+            tinvwl-after-add-to-cart tinvwl-loop-button-wrapper
+          "
+          data-product_id="521"
+        >
+          <div class="tinv-wishlist-clear"></div>
+          <a
+            role="button"
+            tabindex="0"
+            aria-label="Add to Wishlist"
+            class="
+              tinvwl_add_to_wishlist_button
+              tinvwl-icon-heart
+              tinvwl-position-after
+              tinvwl-loop
+            "
+            data-tinv-wl-list="[]"
+            data-tinv-wl-product="521"
+            data-tinv-wl-productvariation="0"
+            data-tinv-wl-productvariations="[0]"
+            data-tinv-wl-producttype="simple"
+            data-tinv-wl-action="add"
+            ><span class="tinvwl_add_to_wishlist-text">Add to Wishlist</span></a
+          >
+          <div class="tinv-wishlist-clear"></div>
+          <div class="tinvwl-tooltip">Add to Wishlist</div>
+        </div>
+      </div>
+
     </div>
     <div v-else>
+
       <h1 class="text-center">No Result Found</h1>
     </div>
   </div>
@@ -1439,6 +1709,7 @@ export default {
     showbigmodalstyle: "",
     showcomparemodal: "",
     showcomparemodalstyle: "",
+
     activeImage: 0,
     //Hold the timeout, so we can clear it when it is needed
     autoSlideTimeout: null,
@@ -1500,8 +1771,19 @@ export default {
       .then((response) => {
         // console.log(response.data.data.data);
         this.results = response.data.data.data;
-        console.log("response.data.data", response.data.data.data);
+        // console.log("response.data.data", response.data.data.data);
         this.isCount = true;
+         const User = {
+            template: '...',
+            created() {
+              this.$watch(
+                () => this.$route.params,
+                (toParams, previousParams) => {
+                  // react to route changes...
+                }
+              )
+            },
+          }
       })
       .catch((error) => {});
   },
