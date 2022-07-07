e12e8c3dc780f2c714fbbb0076ff4953be69423c
AsadKhanAb17 <asadkhan10222@gmail.com>
2022-07-07 17:45:33 +0500

    Design Updated

--- src/views/dashboard/Wishlist.vue
+++ src/views/dashboard/Wishlist.vue
@@ -2,83 +2,96 @@
   <Header />
   <main id="main" class="site-primary">
     <div class="site-content">
-     <div class="shop-content my-account-page mt-60">
+      <div class="shop-content my-account-page mt-60">
         <div class="container">
           <div class="woocommerce">
             <div class="row content-wrapper sidebar-right">
               <div class="col-12 col-md-12 col-lg-12 content-primary">
                 <div class="my-account-wrapper">
                   <Sidebar />
-      <div
-        data-elementor-type="wp-page"
-        data-elementor-id="883"
-        class="elementor elementor-883"
-        data-elementor-settings="[]"
-      >
-        <div class="elementor-section-wrap">
-          <section
-            class="
-              elementor-section
-              elementor-top-section
-              elementor-element
-              elementor-element-17370978
-              elementor-section-boxed
-              elementor-section-height-default
-              elementor-section-height-default
-            "
-            data-id="17370978"
-            data-element_type="section"
-          >
-            <div class="elementor-container elementor-column-gap-extended">
-              <div
-                class="
-                  elementor-column
-                  elementor-col-100
-                  elementor-top-column
-                  elementor-element
-                  elementor-element-651ccb50
-                "
-                data-id="651ccb50"
-                data-element_type="column"
-              >
-                <div class="elementor-widget-wrap elementor-element-populated">
                   <div
-                    class="
-                      elementor-element
-                      elementor-element-19726f3e
-                      elementor-widget
-                      elementor-widget-shortcode
-                    "
-                    data-id="19726f3e"
-                    data-element_type="widget"
-                    data-widget_type="shortcode.default"
+                    data-elementor-type="wp-page"
+                    data-elementor-id="883"
+                    class="elementor elementor-883 w-75"
+                    data-elementor-settings="[]"
                   >
-                    <div class="elementor-widget-container">
-                      <div class="elementor-shortcode">
+                    <div class="elementor-section-wrap">
+                      <section
+                        class="
+                          elementor-section
+                          elementor-top-section
+                          elementor-element
+                          elementor-element-17370978
+                          elementor-section-boxed
+                          elementor-section-height-default
+                          elementor-section-height-default
+                        "
+                        data-id="17370978"
+                        data-element_type="section"
+                      >
                         <div
-                          class="tinv-wishlist woocommerce tinv-wishlist-clear"
+                          class="
+                            elementor-container elementor-column-gap-extended
+                          "
                         >
-                          <div class="tinv-header">
-                            <h2>{{$t('Wishlist')}}</h2>
+                          <div
+                            class="
+                              elementor-column
+                              elementor-col-100
+                              elementor-top-column
+                              elementor-element
+                              elementor-element-651ccb50
+                            "
+                            data-id="651ccb50"
+                            data-element_type="column"
+                          >
+                            <div
+                              class="
+                                elementor-widget-wrap
+                                elementor-element-populated
+                              "
+                            >
+                              <div
+                                class="
+                                  elementor-element
+                                  elementor-element-19726f3e
+                                  elementor-widget
+                                  elementor-widget-shortcode
+                                "
+                                data-id="19726f3e"
+                                data-element_type="widget"
+                                data-widget_type="shortcode.default"
+                              >
+                                <div class="elementor-widget-container">
+                                  <div class="elementor-shortcode">
+                                    <div
+                                      class="
+                                        tinv-wishlist
+                                        woocommerce
+                                        tinv-wishlist-clear
+                                      "
+                                    >
+                                      <div class="tinv-header">
+                                        <h2>{{ $t("Wishlist") }}</h2>
+                                      </div>
+                                      <items />
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
                           </div>
-                          <items />
                         </div>
-                      </div>
+                      </section>
                     </div>
                   </div>
                 </div>
+                <!-- site-content -->
               </div>
             </div>
-          </section>
+          </div>
         </div>
       </div>
-    </div>
-    <!-- site-content -->
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
     </div>
   </main>
   <div>
@@ -90,8 +103,6 @@
 setTimeout(function () {
   document.getElementById("menu-sidebar-menu-1").classList.remove("show");
 }, 500);
-
-
 </script>
 
 <script>
--- src/views/layout/LoginCart.vue
+++ src/views/layout/LoginCart.vue
@@ -258,7 +258,144 @@
           </p>
         </div>
         <div v-else>
-          <h3 class="p-2">{{ $t("Cart_is_Empty") }}</h3>
+          <div class="fl-mini-cart-content" style="opacity: 1">
+            <div
+              class="
+                products
+                column-1
+                mobile-1
+                woocommerce-mini-cart
+                cart_list
+                product_list_widget
+              "
+            >
+              <div
+                class="
+                  cart-slider
+                  swiper-container
+                  klb-mini-cart
+                  swiper-container-initialized
+                  swiper-container-vertical
+                  swiper-container-pointer-events
+                  slider-loaded
+                  swiper-initialized
+                  swiper-vertical
+                  swiper-pointer-events
+                  swiper-backface-hidden
+                "
+                data-effect="slide"
+                data-direction="vertical"
+                data-loop="false"
+                data-speed="1000"
+                data-spacebetween="0"
+                data-autoplay="false"
+                data-autospeed="300"
+                data-items="2"
+                data-mobileitems="2"
+                data-tabletitems="2"
+              >
+                <div class="slider-loader">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="spinner"
+                    width="50"
+                    height="50"
+                    viewBox="0 0 44 44"
+                  >
+                    <circle
+                      class="spinner-path"
+                      cx="22"
+                      cy="22"
+                      r="20"
+                      fill="none"
+                      stroke-width="3"
+                    ></circle>
+                  </svg>
+                </div>
+
+                <div
+                  class="swiper-wrapper cart-items"
+                  id="swiper-wrapper-f78938cd85acf3c8"
+                  aria-live="polite"
+                  style="
+                    transform: translate3d(0px, 0px, 0px);
+                    transition-duration: 0ms;
+                  "
+                >
+                  <div class="cart-empty">
+                    <div class="empty-icon">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                      >
+                        <path
+                          d="M460 504H52c-24 0-44-20-44-44V138.4h496v320.8c0 24.8-20 44.8-44 44.8z"
+                          fill="#FFBD27"
+                        ></path>
+                        <path
+                          d="M52 472c-6.4 0-12-5.6-12-12V170.4h432v288.8c0 6.4-5.6 12-12 12H52v.8z"
+                          fill="#fff"
+                        ></path>
+                        <path
+                          fill="#E1E6E9"
+                          d="M18.4 8h462.4v130.4H18.4z"
+                        ></path>
+                        <g fill="#fff">
+                          <path
+                            d="M504 138.4h-62.4v-64zM480.8 114.4V8l-39.2 66.4zM8 138.4h62.4v-64z"
+                          ></path>
+                          <path d="M18.4 8v120l52-53.6z"></path>
+                        </g>
+                        <g fill="#193651">
+                          <path
+                            d="M512 138.4c0-1.6-.8-4-2.4-5.6l-20.8-21.6V8c0-4.8-4-8-8-8H18.4c-4 0-8 4-8 8v116.8l-8 8.8c-1.6.8-2.4 3.2-2.4 4.8v320.8C0 488.8 23.2 512 52 512h407.2c28.8 0 52-23.2 52-52V138.4h.8zm-60-65.6l20.8-36v57.6L452 72.8zm20.8 44.8v12.8h-23.2V93.6l23.2 24zM467.2 16l-32 53.6c-.8.8-.8 3.2-.8 4v56.8h-356v-56c0-1.6-.8-3.2-1.6-4.8L35.2 16h432zM26.4 31.2l32.8 42.4L26.4 108V31.2zm36 62.4v36.8h-36l36-36.8zM496 460c0 20-16 36-36 36H52c-20 0-36-16-36-36V146.4h480V460z"
+                          ></path>
+                          <path
+                            d="M372.8 429.6h16v16h-16zM428.8 429.6h16v16h-16zM67.2 429.6h16v16h-16zM123.2 429.6h16v16h-16zM140.8 264.8c5.6 89.6 55.2 160 115.2 160s108.8-70.4 115.2-160c5.6-2.4 9.6-8.8 9.6-15.2 0-9.6-8-17.6-17.6-17.6s-17.6 8-17.6 17.6c0 6.4 3.2 12 8.8 15.2-5.6 81.6-48 144.8-99.2 144.8s-93.6-63.2-99.2-144.8c4.8-3.2 8.8-8.8 8.8-15.2 0-9.6-8-17.6-17.6-17.6s-17.6 8-17.6 17.6c1.6 6.4 5.6 12 11.2 15.2z"
+                          ></path>
+                        </g>
+                      </svg>
+                    </div>
+                    <!-- empty-icon -->
+                    <div class="empty-text">No products in the cart.</div>
+                  </div>
+                  <!-- cart-empty -->
+                </div>
+                <div
+                  class="
+                    swiper-button-prev swiper-button-lock swiper-button-disabled
+                  "
+                  tabindex="-1"
+                  role="button"
+                  aria-label="Previous slide"
+                  aria-controls="swiper-wrapper-f78938cd85acf3c8"
+                  aria-disabled="true"
+                ></div>
+                <div
+                  class="
+                    swiper-button-next swiper-button-lock swiper-button-disabled
+                  "
+                  tabindex="-1"
+                  role="button"
+                  aria-label="Next slide"
+                  aria-controls="swiper-wrapper-f78938cd85acf3c8"
+                  aria-disabled="true"
+                ></div>
+                <span
+                  class="swiper-notification"
+                  aria-live="assertive"
+                  aria-atomic="true"
+                ></span>
+                <span
+                  class="swiper-notification"
+                  aria-live="assertive"
+                  aria-atomic="true"
+                ></span>
+              </div>
+            </div>
+
+            <div id="view-cart-checkout"></div>
+          </div>
         </div>
 
         <!-- cart-noticy -->
